4a9b260fa413861160c50c58e950e598b8acee36
cyruswere <cyrus.were@student.moringaschool.com>
2021-07-22 09:54:27 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
+import { EngineComponent } from './engine/engine.component';
+import { WheelsComponent } from './wheels/wheels.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    EngineComponent,
+    WheelsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
